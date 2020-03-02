ac427345dbc64a5cc6683bb38ec43443561205ce
Laura Marsh <lau.lyn.marsh@gmail.com>
2020-03-02 11:48:11 -0800

    added about blurb

--- fancy.js
+++ fancy.js
@@ -1,25 +1,57 @@
-
 //Run the title screen typewriter effect
-setTimeout( () => {
-  typeWriter("line-1", " hi");
+const titleText = [
+  " hi",
+  " my name is Laura ~",
+  " i'm a software & ui engineer!",
+  " scroll down :)"
+];
+
+setTimeout(() => {
+  typeWriter("line-1", titleText[0]);
 }, 1500);
-setTimeout( () => {
-  typeWriter("line-2", " my name is Laura :)");
+setTimeout(() => {
+  typeWriter("line-2", titleText[1]);
 }, 2700);
-setTimeout( () => {
-  typeWriter("line-3", " i'm a software & ui engineer!");
+setTimeout(() => {
+  typeWriter("line-3", titleText[2]);
 }, 5000);
+setTimeout(() => {
+  typeWriter("line-4", titleText[3]);
+}, 9500);
 
 function typeWriter(id, text) {
-  const line = $(`#${id}`); 
+  const line = $(`#${id}`);
   if (text.length) {
     line.text(line.text() + text[0]);
-    setTimeout(() => typeWriter(id, text.substring(1)), id === "line-1" ? 200 : 80);
+    setTimeout(
+      () => typeWriter(id, text.substring(1)),
+      id === "line-1" ? 200 : 80
+    );
   } else {
-    if (id !== "line-3") {
+    if (parseInt(id.slice(-1)) !== titleText.length) {
       line.removeClass("cursor");
       $(`#line-${parseInt(id.slice(-1)) + 1}-ds`).removeClass("hidden");
       $(`#line-${parseInt(id.slice(-1)) + 1}`).addClass("cursor");
-    } 
+    }
   }
 }
+
+// plenty o' event listeners for the scroll behavior
+//  on scroll down, things are supposed to switch in line
+let lastScrollTop = 0;
+const sectionOrder = ["title", "about"];
+
+$(document).ready(function() {
+  $(document).scroll(function() {
+    console.log("insideee");
+    let st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+      // downscroll code
+      console.log("DOWNNNNN");
+    } else {
+      // upscroll code
+      console.log("UPPPPPPP");
+    }
+    lastScrollTop = st;
+  });
+});
